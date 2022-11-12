9ed9cd81c1cad0678e9d4b778f02e54d3ce64a42
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-11-12 23:25:30 +0100

    Actualizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import CreateEvent from './components/CreateEvent';
+import Event from './components/Event';
+import navOptions from './data/navOptions';
 import './App.css';
 
 function App() {
+  let navbarOpen = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="main-container">
+        <div className="nav-container" style={navbarOpen ? {} : {left : "-20vw"}>
+          <div className="nav-logo">
+            <img src="../assets/logo.png" alt="logo" />
+          </div>
+          <div className="nav-options">
+            {navOptions.map((option : any, index : any) => (
+              <Link to={option.path} key={index}>
+                <div className={"nav-option" +
+                  option.auth ? 'nav-option-auth' : 'nav-option-unauth'}>
+                  <img height={10} src={option.icon} alt={option.name} />
+                  <p>{option.name}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      <div className="content-container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/create-event" element={<CreateEvent />}></Route>
+            <Route path="/event" element={<Event />}></Route>
+          </Routes>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/CreateEvent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const CreateEvent: React.FC = () => {
+    return (
+        <div>
+            CreateEvent
+        </div>
+    )
+}
+export default CreateEvent
