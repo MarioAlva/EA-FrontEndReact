aadd9127df136abc163b9f4709f9da18ea8e773e
OscarPlanas <casplanas7@gmail.com>
2022-12-08 23:18:03 +0100

    Profile v0.3

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../css/Profile.css'
 import { useNavigate } from "react-router-dom"
-import { useSearchParams } from "react-router-dom"
 import * as userService from '../services/UserServices'
 import { useEffect, useState } from 'react'
 import { User } from '../models/User'
@@ -10,32 +9,14 @@ import { User } from '../models/User'
 
 
 const Profile: React.FC = () => {
-	//let series : any[] = [];
     const navigate = useNavigate();
     const handleClick = () => navigate('/updateuser');
-    let username : string = "";
-    let us : string = "";
-    //let user : any;
-    let userProfile : User[] = [];
     const [user, setUser] = useState<User>();
 
 	const loadUser = async () => {
 		const user = await userService.getProfile();
-        console.log(user);
-        console.log("+++++++++");
-        console.log(user.data.username);
-        username = user.data.username;
-        console.log("------------");
-        console.log(username);
         const getUser = user.data as User;
         setUser(getUser);
-
-        console.log(userProfile);
-        console.log("bbbbb");
-        console.log(userProfile[0].username);
-        const us = userProfile[0].username;
-        console.log(us);
-        
 	  }
 	useEffect(() => {
 		loadUser()
@@ -48,25 +29,7 @@ const Profile: React.FC = () => {
 			</div>                        
             <div className="profile-titles">{user?.username}</div>
             <button className="profile-config-button" onClick={handleClick}>Configuration</button>
-
-            {/* <div className="profile-titles">AAA</div> */}
-
-				{/* <div style={{width: "50%", overflowY: "scroll", overflowX: "hidden"}}>
-					{userProfile.map((user) => (
-                        <div className="profile-titles">¨{user.username}</div>
-						 <div className="eventscreen-card">
-						 	<div style={{width: "100%"}}>
-                                 <div className="profile-titles">¨{user.username}</div>
-						 		 <div className="eventscreen-card-title">{event.title}</div> 
-						 		 <div className="eventscreen-card-description">{event.description}</div>
-						 	</div>
-						</div>
-					))}
-
-				</div>
-				<div style={{width: "50%"}}>
-				</div> */}
-			</div>
+		</div>
         
             
     )
--- src/components/UpdateUser.tsx
+++ src/components/UpdateUser.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import '../css/Profile.css'
-import * as ProfileService from '../services/ProfileServices'
-import { useSearchParams } from "react-router-dom";
-import * as UserService from '../services/UserServices'
 
+import { useNavigate } from "react-router-dom"
+import * as userService from '../services/UserServices'
+import { useEffect, useState } from 'react'
+import { User } from '../models/User'
+import Moment from 'react-moment'
+import '../css/UpdateUser.css'
 
 type UpdateForm = {
     username: String;
@@ -14,45 +16,52 @@ type UpdateForm = {
 const UpdateUser: React.FC = () => {
     // let series : any[] = [];
     let clickCreateEvent = true
-    const [searchParams] = useSearchParams();
-    
-    const loadUser = async () => {
-        const user = localStorage.getItem('token');
-        if (!user) { return; }
-        const base64Url = user.split('.')[1];
-        const base64 = base64Url.replace('-', '+').replace('_', '/');
-        const toke = JSON.parse(window.atob(base64));
-        const idUser = toke.id;
-        console.log(toke);
-        console.log(toke.id);
-		//const user1 = await UserService.getProfile(idUser);
-  	};
+    const [user, setUser] = useState<User>();
+    const navigate = useNavigate();
+    const handleClick = () => navigate('/profile');
+	const loadUser = async () => {
+		const user = await userService.getProfile();
+        const getUser = user.data as User;
+        setUser(getUser);
+	  }
+	useEffect(() => {
+		loadUser()
+	  }, [])
 
-	//useEffect(() => {
-	//	loadSeries();
-	//}, []);
-
-    /*const sendEvent = handleSubmit(async (values) => {
-        const res = await UserService.getProfile(values);
-        console.log(res);
-
-        navigate('/event');
-    });*/
-    
+    const sendCreateEvent = () => {
+        clickCreateEvent = !clickCreateEvent
+        console.log(clickCreateEvent)
+    }
 
     return (
-        <div className="create-event-container">
-    		<form action="createEvent" className="create-event" style={clickCreateEvent ? {marginLeft: "0vw", paddingBottom: "20px"} : {paddingBottom: "20px", width: "450px"}}  >
-            <span className="create-event-header">Create Event</span>
-                <label style={{marginBottom: "20px"}}>Title:<input type="text" placeholder="Title"/></label>
-                
-                <label style={{marginBottom: "20px"}}>Description:<input type="text" placeholder="Description"/></label>
-                
-                <label style={{marginBottom: "20px"}} htmlFor="regUsername">Date:<input type="date"/></label>
-                
+        <div className="update-user-container">
+    		<form action="UpdateUser" className="update-user" style={clickCreateEvent ? {marginLeft: "0vw", paddingBottom: "20px"} : {paddingBottom: "20px", width: "450px"}}  >
+            <span className="update-user-header">Profile</span>
+                <p>
+                    <strong>Name: </strong>{user?.name} 
+                </p>
+                <p>
+                    <strong>Username: </strong>{user?.username}
+                </p>
+                <p>
+                    <strong>Email: </strong>{user?.email}
+                </p>
+                <p>
+                    <strong>Birthdate: </strong>
+                    <Moment format="D MMM YYYY" withTitle>{user?.birthdate}</Moment>
+
+                    {/* <Moment fromNow ago>{event.date?.toLocaleString("en-US")}</Moment> */}
+
+                    {/* <strong>Birthdate: </strong>{user?.birthdate} */}
+                </p>
+                <button className="update-user-button" onClick={() => sendCreateEvent()}><b>Update Profile</b></button>
             <div style={{width: "62%", display: "inline-flex", justifyContent: "center", marginBottom: "20px"}}>
                 <div style={{marginRight: "4%", display: "flex", flexDirection: "column", width: "62%"}}>
                 </div>
+            </div>
+            {/* <div className="back-button" style={registerView || forgot ? {marginRight: "280vw"} : {}} onClick={() => {setRegister(false); setForgot(false)}}>
+            </div> */}
+            <div className="back-button" style={{marginRight: "280vw"} }onClick={handleClick}>
             </div>
             {/* <button className="create-event-button" onClick={() => sendCreateEvent()}><b>Crear Evento</b></button> */}
         </form>
