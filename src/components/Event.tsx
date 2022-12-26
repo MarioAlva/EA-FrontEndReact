6d3c16a9c59a0c0a9ce9a019bcc311622dfda94b
MarioAlva <mario.antonio.alva@estudiantat.upc.edu>
2022-12-26 17:38:45 +0100

    Rating + comments

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -7,11 +7,35 @@ import { Event } from '../models/Event'
 import axios from 'axios'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import L from 'leaflet';
+import { width } from '@mui/system'
 
 
 
 
 const Eventpage: React.FC = () => {
+	const comments = [
+		{
+			comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc.",
+			valoration: 5,
+		},
+		{
+			comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc.",
+			valoration: 4,
+		},
+		{
+			comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc.",
+			valoration: 3,
+		},
+		{
+			comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc.",
+			valoration: 2,
+		},
+		{
+			comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc. Sed euismod, nisl nec ultricies lacinia, nunc nisl ultricies nisl, nec ultricies nisl nunc vel nunc.",
+			valoration: 1,
+		},
+	]
+
 	const iconPerson = new L.Icon({
 		iconUrl: require('../assets/img/pin.png'),
 		iconRetinaUrl: require('../assets/img/pin.png'),
@@ -28,6 +52,11 @@ const Eventpage: React.FC = () => {
 	useEffect(() => {
 		// loadEvent(id)
 	  }, [])
+	function setRate (rate: string){
+		document.documentElement.style.setProperty('--start', rate);
+		console.log(rate);
+		return;
+	}
     return (
 		<div className="event-container">
 			<div className="event-header">
@@ -52,16 +81,32 @@ const Eventpage: React.FC = () => {
 							url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 						/>
 						<Marker icon={iconPerson} position={[41.275188, 1.986412]}>
-							<Popup>
-								A pretty CSS3 popup. <br /> Easily customizable.
-							</Popup>
 						</Marker>
 					</MapContainer>
 				</div>
 			</div>
 			<div className="event-footer">
 				<div className="event-valoration"></div>
-				<div className="event-comments"></div>
+				<div className="event-comments">
+					<span className='comments-title'>Comments</span>
+					{comments.map((comment, index) => {
+						return (
+							<div className="comment-container">
+								<div className="comment-body">
+									<span>{comment.comment}</span>
+								</div>
+								<div className="comment-footer">
+									<div className="comment-valoration">
+										<div className='comment-rating'>
+										</div>
+									</div>
+									<span className="comment-author">Author</span>
+								</div>
+							</div>
+						)
+					})
+					}
+				</div>
 			</div>
 		</div>
 			
