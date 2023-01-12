58681ea80b48019ea0d7bd00df36560fb30708e3
pdepeo <pabloramosbarahona@gmail.com>
2023-01-12 19:28:07 +0100

    final commit
    entrega

--- src/components/UpdateUser.tsx
+++ src/components/UpdateUser.tsx
@@ -6,9 +6,48 @@ import { useEffect, useState } from 'react'
 import { User } from '../models/User'
 import Moment from 'react-moment'
 import '../css/UpdateUser.css'
+import MyDropzone from './MyDropZone'
 
 
+const MyForm = () => {
+    const [fileData, setFileData] = useState<any>()
+    const fileSelectedHandler = (event: any) => {
+        let file = event.target.files[0].name;
+        setFileData({
+            selectedFile: event.target.files[0],
+            filename: event.target.value
+        })
+    }
 
+    const fileUploadHandler = (event: any) => {
+        let formData = new FormData();
+        formData.append('filename', fileData.filename);
+        formData.append('file', fileData.selectedFile);
+
+        fetch('http://localhost:5432/api/users/update-image/63c03a4b12436d8c8cf03813', {
+            method: 'POST',
+            body: formData,
+            // headers: { 'content-type': 'multipart/form-data' }
+        }).then(res => res.json())
+        .then(res => console.log())
+    }
+
+    return (<div>
+        <input
+            type="file"
+            name="file"
+            id="file"
+            placeholder="Upload your file"
+            onChange={fileSelectedHandler}
+        />
+        <br />
+        <button
+            type="submit"
+            onClick={fileUploadHandler}
+        >Upload pick</button>
+
+    </div>)
+}
 const UpdateUser: React.FC = () => {
     // let series : any[] = [];
     let clickUpdate = true
@@ -16,23 +55,24 @@ const UpdateUser: React.FC = () => {
     const navigate = useNavigate();
     const handleClick = () => navigate('/profile');
     const handleClick2 = () => navigate('/updateUserValues');
-	const loadUser = async () => {
-		const user = await userService.getProfile();
-        
+    const loadUser = async () => {
+        const user = await userService.getProfile();
+
         const getUser = user.data as User;
         setUser(getUser);
-	  }
-	useEffect(() => {
-		loadUser()
-	  }, [])
+    }
+    useEffect(() => {
+        // loadUser()
+    }, [])
 
+    const [selectedFile, setSelectedFile] = useState()
 
     return (
         <div className="update-user-container">
-    		<form action="UpdateUser" className="update-user" style={clickUpdate ? {marginLeft: "0vw", paddingBottom: "20px"} : {paddingBottom: "20px", width: "450px"}}  >
-            <span className="update-user-header">Profile</span>
+            <form action="UpdateUser" className="update-user" style={clickUpdate ? { marginLeft: "0vw", paddingBottom: "20px" } : { paddingBottom: "20px", width: "450px" }}  >
+                <span className="update-user-header">Profile</span>
                 <p>
-                    <strong>Name: </strong> {user?.name} 
+                    <strong>Name:</strong> {user?.name}
                 </p>
                 <p>
                     <strong>Username: </strong>{user?.username}
@@ -48,18 +88,29 @@ const UpdateUser: React.FC = () => {
 
                     {/* <strong>Birthdate: </strong>{user?.birthdate} */}
                 </p>
-                <button className="update-user-button" onClick={handleClick2}><b>Update Profile</b></button>
-            <div style={{width: "62%", display: "inline-flex", justifyContent: "center", marginBottom: "20px"}}>
-                <div style={{marginRight: "4%", display: "flex", flexDirection: "column", width: "62%"}}>
+
+
+                <div className='update-image'>
+                    <MyForm />
                 </div>
-            </div>
-            {/* <div className="back-button" style={registerView || forgot ? {marginRight: "280vw"} : {}} onClick={() => {setRegister(false); setForgot(false)}}>
+
+
+
+
+
+
+                <button className="update-user-button" onClick={handleClick2}><b>Update Profile</b></button>
+                <div style={{ width: "62%", display: "inline-flex", justifyContent: "center", marginBottom: "20px" }}>
+                    <div style={{ marginRight: "4%", display: "flex", flexDirection: "column", width: "62%" }}>
+                    </div>
+                </div>
+                {/* <div className="back-button" style={registerView || forgot ? {marginRight: "280vw"} : {}} onClick={() => {setRegister(false); setForgot(false)}}>
             </div> */}
-            <div className="back-button" style={{marginRight: "280vw"} }onClick={handleClick}>
-            </div>
-            {/* <button className="create-event-button" onClick={() => sendCreateEvent()}><b>Crear Evento</b></button> */}
-        </form>
-    </div>		
+                <div className="back-button" style={{ marginRight: "280vw" }} onClick={handleClick}>
+                </div>
+                {/* <button className="create-event-button" onClick={() => sendCreateEvent()}><b>Crear Evento</b></button> */}
+            </form>
+        </div>
     )
 }
 export default UpdateUser
